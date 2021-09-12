7695c08d6780b9a143edf928181498c40f2cea52
Zeeenx <87702906+Zeeenx@users.noreply.github.com>
2021-09-12 19:27:35 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,14 +64,7 @@ async def start(c, m, cb=False):
             caption += f"__💬 DC ID:__ {channel.dc_id}\n\n" if channel.dc_id else ""
             caption += f"__👁 Members Count:__ {channel.members_count}\n\n" if channel.members_count else ""
 
-        else: #if file not from channel
-            user = await c.get_users(int(chat_id))
-            caption += "**--Uploader Detailseee:--**\n\n"
-            caption += f"__🦚 First Name:__ `{user.first_name}`\n\n"
-            caption += f"__🐧 Last Name:__ `{user.last_name}`\n\n" if user.last_name else ""
-            caption += f"__👁 User Name:__ @{user.username}\n\n" if user.username else ""
-            caption += f"__👤 User Id:__ `{user.id}`\n\n"
-            caption += f"__💬 DC ID:__ {user.dc_id}\n\n" if user.dc_id else ""
+
 
         await msg.copy(m.from_user.id, caption=caption)
 
